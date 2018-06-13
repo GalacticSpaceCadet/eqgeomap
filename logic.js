7832cff3544a5465c8a43123ca0d36254ef2a5f6
Di Gandy <dileegan@icloud.com>
2018-06-13 01:49:20 -0500

    fixed ICON404 causing slow render

--- logic.js
+++ logic.js
@@ -1,10 +1,71 @@
 //Set variable to hold the Query URL in Global Scope
 
-var queryURL ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var myToken = config.mapboxKey
+
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Perform a GET request to the query URL
 d3.json(queryURL, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-console.log(data.features);
- });
+    // Once we get a response, send the data.features object to the createFeatures function
+    //console.log(data.features);
+    createFeatures(data.features);
+});
 
+function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>"
+            + "<hr><p>" + feature.properties.mag + "</p>");
+    }
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+       // Added this bad boi!
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng)
+        },
+        onEachFeature: onEachFeature
+    });
+
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    // Define streetmap and darkmap layers
+    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=" + myToken);
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=" + myToken);
+
+    // Define a baseMaps object to hold our base layers satellite-v9
+    var baseMaps = {
+        "Light Map": lightMap,
+        "Dark Map": darkmap
+    };
+
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+        center: [
+            37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [lightMap, earthquakes]
+    });
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
